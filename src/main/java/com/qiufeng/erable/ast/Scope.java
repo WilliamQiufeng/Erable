2e1c268f0ae7a8e0e1b49a359c59c44bcc6d0e49
Qiufeng54321 <williamcraft@163.com>
2019-03-31 10:50:23 +0800

    Added:  + Exception  + EListener  + Scope

---
+++ src/main/java/com/qiufeng/erable/ast/Scope.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright (C) 2019 Qiufeng54321
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.qiufeng.erable.ast;
+
+/**
+ *
+ * @author Qiufeng54321
+ */
+public class Scope extends Code {
+    public Scope(Code parent){
+	super("scope",parent);
+	this.tag+=id;
+    }
+    @Override
+    public String write() {
+	throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+}
