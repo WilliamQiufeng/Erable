5bacff7abb947eb6c6e65b58d21c28da0fb98555
Tanpero <3185750139@qq.com>
2019-05-28 21:15:38 +0800

    reset all

--- ErableLang/Compiler/main.cpp
+++ ErableLang/Compiler/main.cpp
@@ -3,13 +3,7 @@
 
 int main(int argc, char* argv[]) {
     using namespace Erable::Compiler;
-	
-#ifndef TANPERO_BUILD
-	Lexer lexer = Lexer("/williamye/program/antlr/erable/ErableLang/tests/lex.erable");
-#else
-	Lexer lexer = Lexer("D:\lex.erable");
-#endif
-
+    Lexer lexer = Lexer("/williamye/program/antlr/erable/ErableLang/tests/lex.erable");
     lexer.lex();
     for (auto token : lexer.getTokens()) {
         std::cout << "Token " << token.name << "[" << token.data << "]" << std::endl;
